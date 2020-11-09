95e861e9ca2434b0a59f885d22d3120db0a4e02a
sarazafe <sara@blunder.es>
2020-11-09 20:22:45 +0100

    Fix key

--- src/User.js
+++ src/User.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const User = ({username, numGamesPlayed}) => {
-	return <p key={username}>{username} played {numGamesPlayed} games</p>;
+	return <p>{username} played {numGamesPlayed} games</p>;
 }
--- src/UsersList.js
+++ src/UsersList.js
@@ -4,7 +4,7 @@ import {User} from "./User";
 export const UsersList = ({users}) => {
 	return (
 		<div>
-			{users.map(user => <User username={user.username} numGamesPlayed={user.numGamesPlayed}/>)}
+			{users.map(user => <User key={user.username} username={user.username} numGamesPlayed={user.numGamesPlayed}/>)}
 		</div>
 	)
 };
